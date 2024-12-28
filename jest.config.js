fb0a57c6cefbe663c92f9020acb8a37c18672d72
Alex Vergara <alex.vergara.dev@gmail.com>
2024-12-28 13:16:13 +0100

    ci: introducing tests with jest for the actions-scripts. Added a workflow to run them

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// Base configuration for using 'JEST' as our test suite for this repo 'JS' custom scripts
+// that typically we use in our actions
+
+export default {
+    transform: {}, // Disable default Babel transform
+    testEnvironment: 'node', // Default to Node.js environment
+};
+  
